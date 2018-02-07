7756e8744f28cd79c03edbdada458466e89dbd6d
Justin K <justin-kim1@hotmail.com>
2018-02-07 07:18:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,3 @@
-# def roll_call_dwarves# code an argument here
-#   # Your code here
-# end
-
-# def summon_captain_planet# code an argument here
-#   # Your code here
-# end
-
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
-
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
-
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
     index += 1
